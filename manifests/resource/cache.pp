b8ba2799e847f2e214050c47532df7416f3bda6f
Martin Loy <martinloy.uy@gmail.com>
2013-09-29 14:14:28 -0300

    add cache resource

---
+++ manifests/resource/cache.pp
@@ -0,0 +1,40 @@
+# define: nginx::resource::cache
+#
+# This definition creates a new cache_zone entry for NGINX
+#
+# Parameters:
+#   [*ensure*]      - Enables or disables the specified cache zone (present|absent)
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage (storage dir must exist):
+#  nginx::resource::cache { 'domain1':
+#    ensure  => present,
+#    storage_dir => '/var/nginx-cache'
+#  }
+define nginx::resource::cache (
+  $ensure      = 'present',
+  $storage_dir = '/var/nginx-cache',
+  $levels      = "1:2",
+  $keys_zone   = "${name}:8m",
+  $inactive    = "7d",
+  $max_size    = "1g"
+) {
+  File {
+    owner => 'root',
+    group => 'root',
+    mode  => '0644',
+  }
+
+  file { "/etc/nginx/conf.d/${name}-cache_zone.conf":
+    ensure   => $ensure ? {
+      'absent' => absent,
+      default  => 'file',
+    },
+    content  => "proxy_cache_path ${storage_dir}/${name} levels=${levels} keys_zone=${keys_zone} inactive=${inactive} max_size=${max_size};\n",
+    notify   => Class['nginx::service'],
+  }
+
+}
